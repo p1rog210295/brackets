97aed237ce916b928313ca013f387c34356929fd
Evgeniy Deineka <nescafe210295@gmail.com>
2021-03-16 00:17:21 +0300

    feat: add checker brackets function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+module.exports =
+    function check(str, bracketsConfig) {
+        brackets = new Map();
+
+        for (config of bracketsConfig) {
+            brackets.set(config[0], config[1]);
+        }
+
+        stack = [];
+
+        for (symbol of str) {
+            if (stack.length > 0) {
+                let closingBracket = brackets.get(stack[stack.length - 1]);
+
+                if (symbol === closingBracket) {
+                    stack.pop();
+                    continue;
+                }
+            }
+
+            if (brackets.has(symbol)) {
+                stack.push(symbol);
+                continue;
+            }
+            return false;
+        }
+        return stack.length === 0;
+
+    }
